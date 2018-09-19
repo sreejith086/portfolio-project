c51410f6740299e02263954a6ce4d40b0b7550ae
sreejith086 <trulysreejith@gmail.com>
2018-09-19 07:15:13 +0530

    Home page and templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=60)
+    pub_date=models.DateTimeField(default=timezone.now)
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
